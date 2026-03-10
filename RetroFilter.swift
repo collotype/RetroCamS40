560c9d1eb4580a04848b1873cdc96e18810d2918
collotype <2008ezra2008@bk.ru>
2026-03-10 16:59:26 +0300

    Create RetroFilter.swift

---
+++ RetroFilter.swift
@@ -0,0 +1,13 @@
+import Foundation
+import CoreImage
+import UIKit
+
+enum RetroFilter {
+    static func makeRetroPhoto(from image: UIImage, addDateStamp: Bool) -> UIImage {
+        image
+    }
+
+    static func applyToVideoFrame(_ image: CIImage) -> CIImage {
+        image
+    }
+}
