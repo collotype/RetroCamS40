aa5020745bbba15d396834ea7f2f217b94e5687a
collotype <2008ezra2008@bk.ru>
2026-03-10 17:41:11 +0300

    Update CameraService.swift

--- CameraService.swift
+++ CameraService.swift
@@ -1,1 +1,16 @@
-CameraService.swift
+import Foundation
+import AVFoundation
+
+final class CameraService: NSObject, ObservableObject {
+    @Published var isRecording: Bool = false
+    @Published var useRetroFilter: Bool = true
+    @Published var addDateStamp: Bool = true
+
+    let session = AVCaptureSession()
+
+    func startIfNeeded() {}
+    func stop() {}
+    func switchCamera() {}
+    func takePhoto() {}
+    func toggleRecording() {}
+}
