387a3fec0ad2e7854a20272cb74935d5803aadd4
collotype <2008ezra2008@bk.ru>
2026-03-12 02:15:35 +0300

    Update CameraPreview.swift

--- CameraPreview.swift
+++ CameraPreview.swift
@@ -1,24 +1,56 @@
 import SwiftUI
 import AVFoundation
+import UIKit
 
 struct CameraPreview: UIViewRepresentable {
     let session: AVCaptureSession
 
     func makeUIView(context: Context) -> PreviewView {
         let view = PreviewView()
-        view.videoPreviewLayer.session = session
-        view.videoPreviewLayer.videoGravity = .resizeAspectFill
+        view.backgroundColor = .black
+        view.previewLayer.videoGravity = .resizeAspectFill
+        view.previewLayer.session = session
+        view.previewLayer.connection?.videoOrientation = .portrait
         return view
     }
 
     func updateUIView(_ uiView: PreviewView, context: Context) {
+        if uiView.previewLayer.session !== session {
+            uiView.previewLayer.session = session
+        }
+
+        uiView.previewLayer.videoGravity = .resizeAspectFill
+
+        if let connection = uiView.previewLayer.connection,
+           connection.isVideoOrientationSupported {
+            connection.videoOrientation = .portrait
+        }
     }
 }
 
 final class PreviewView: UIView {
-    override class var layerClass: AnyClass { AVCaptureVideoPreviewLayer.self }
+    override class var layerClass: AnyClass {
+        AVCaptureVideoPreviewLayer.self
+    }
 
-    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+    var previewLayer: AVCaptureVideoPreviewLayer {
         layer as! AVCaptureVideoPreviewLayer
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        clipsToBounds = true
+        backgroundColor = .black
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        clipsToBounds = true
+        backgroundColor = .black
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        previewLayer.frame = bounds
     }
 }
